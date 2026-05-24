8fc09b47f7e987db3034273e63d6fb60cd04618d
Harry Holcomb <31421567+holcombharry@users.noreply.github.com>
2026-05-24 16:26:56 -0700

    Add rookie projections

--- src/sgf_model/projections/__init__.py
+++ src/sgf_model/projections/__init__.py
@@ -2,5 +2,18 @@
 
 from sgf_model.projections.player import project_players
 from sgf_model.projections.regression import fit_regression_priors
+from sgf_model.projections.rookie import (
+    build_rookie_training_data,
+    fit_rookie_models,
+    make_rookie_class_from_history,
+    project_rookies,
+)
 
-__all__ = ["fit_regression_priors", "project_players"]
+__all__ = [
+    "build_rookie_training_data",
+    "fit_regression_priors",
+    "fit_rookie_models",
+    "make_rookie_class_from_history",
+    "project_players",
+    "project_rookies",
+]
